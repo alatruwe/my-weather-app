08a75579609c2f0c3f3999e418c7edbb2d27b695
alatruwe <49768573+alatruwe@users.noreply.github.com>
2021-09-30 15:10:05 -0700

    add data display logic for WeatherCity component

--- client/src/components/Weather/WeatherCity/WeatherCity.js
+++ client/src/components/Weather/WeatherCity/WeatherCity.js
@@ -1,12 +1,20 @@
 import { Component } from "react";
 import data from "../../../data.json";
+import icons from "../../../weather-codes-icon-map.json";
 
 class WeatherCity extends Component {
   render() {
+    //get icon name
+    const id = data.list[0].weather[0].icon;
+    // get image name from icon
+    const weatherImage = icons;
+    // build image source
+    const src = "../../../assets/" + weatherImage[id];
+
     return (
       <div>
         <h1>{data.city.name}</h1>
-
+        <img src={src} alt="none" />
         <p>{data.list[0].main.temp}</p>
       </div>
     );
