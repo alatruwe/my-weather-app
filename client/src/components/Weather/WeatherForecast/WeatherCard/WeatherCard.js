2c125e9b61298f74ddab37b18e7bc44bd030ba78
alatruwe <49768573+alatruwe@users.noreply.github.com>
2021-10-01 12:57:46 -0700

    add WeatherCard component + light styling

---
+++ client/src/components/Weather/WeatherForecast/WeatherCard/WeatherCard.js
@@ -0,0 +1,88 @@
+import { Component } from "react";
+
+class WeatherCard extends Component {
+  // use state to watch for click or not
+  constructor(props) {
+    super(props);
+    this.state = {
+      showDetails: false,
+    };
+  }
+
+  showDetails = () => {
+    if (this.state.showDetails) {
+      this.setState({ showDetails: false });
+    } else {
+      this.setState({ showDetails: true });
+    }
+  };
+
+  renderDetails = () => {
+    return (
+      <>
+        <div className="weatherDetails">
+          <div className="weatherDetailsIcon">
+            <i className="fas fa-temperature-low"></i>
+            <p>Lowest</p>
+          </div>
+
+          <p>{this.props.min} F</p>
+        </div>
+        <div className="weatherDetails">
+          <div className="weatherDetailsIcon">
+            <i className="fas fa-temperature-high"></i>
+            <p>Highest</p>
+          </div>
+
+          <p>{this.props.max} F</p>
+        </div>
+        <div className="weatherDetails">
+          {" "}
+          <div className="weatherDetailsIcon">
+            <i className="fas fa-thermometer-half"></i>
+            <p>Feels like</p>
+          </div>
+          <p>{this.props.feelsLike} F</p>
+        </div>
+        <div className="weatherDetails">
+          <div className="weatherDetailsIcon">
+            <i className="fas fa-tint"></i>
+            <p>Humidity</p>
+          </div>
+          <p>{this.props.humidity} %</p>
+        </div>
+        <div className="weatherDetails">
+          <div className="weatherDetailsIcon">
+            <i className="fas fa-wind"></i>
+            <p>Wind speed</p>
+          </div>
+
+          <p>{this.props.wind} mph</p>
+        </div>
+      </>
+    );
+  };
+
+  render() {
+    return (
+      <div onClick={this.showDetails}>
+        <p>{this.props.icon}</p>
+        <p>{this.props.description}</p>
+        <div>
+          <p>{this.props.temp} F</p>
+        </div>
+        <div className="weatherDetails">
+          <div className="weatherDetailsIcon">
+            <i className="fas fa-sun"></i>
+            <p>UV index</p>
+          </div>
+          <p>UV index</p>
+        </div>
+
+        <div>{this.state.showDetails ? this.renderDetails() : <></>}</div>
+      </div>
+    );
+  }
+}
+
+export default WeatherCard;
--- client/src/components/Weather/WeatherForecast/WeatherForecast.js
+++ client/src/components/Weather/WeatherForecast/WeatherForecast.js
@@ -1,11 +1,32 @@
 import { Component } from "react";
 import data from "../../../data.json";
+import WeatherCard from "./WeatherCard/WeatherCard";
+import "./WeatherForecast.css";
 
 class WeatherForecast extends Component {
+  // get data from context here
+  // pass down data as props to WeatherCard
   render() {
     return (
       <div>
-        <h1>5 Days go here</h1>
+        <ul className="weatherForecast">
+          {data.list.slice(1).map((data, index) => (
+            <li key={data.dt}>
+              <h2>Day {index + 2}</h2>
+
+              <WeatherCard
+                icon={data.weather[0].icon}
+                description={data.weather[0].description}
+                temp={data.main.temp}
+                min={data.main.temp_min}
+                max={data.main.temp_max}
+                feelsLike={data.main.feels_like}
+                humidity={data.main.humidity}
+                wind={data.wind.speed}
+              />
+            </li>
+          ))}
+        </ul>
       </div>
     );
   }
