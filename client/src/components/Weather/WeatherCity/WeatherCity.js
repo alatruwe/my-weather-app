eecf6a417df4ce7e895c44675398560a272149e8
alatruwe <49768573+alatruwe@users.noreply.github.com>
2021-10-01 11:09:07 -0700

    refactor WeatherCity to use services

--- client/src/components/Weather/WeatherCity/WeatherCity.js
+++ client/src/components/Weather/WeatherCity/WeatherCity.js
@@ -1,15 +1,10 @@
 import { Component } from "react";
 import data from "../../../data.json";
-import icons from "../../../weather-codes-icon-map.json";
+import getIconSource from "../../../services/get-icon-source";
 
 class WeatherCity extends Component {
   render() {
-    //get icon name
-    const id = data.list[0].weather[0].icon;
-    // get image name from icon
-    const weatherImage = icons;
-    // build image source
-    const src = "../../../assets/" + weatherImage[id];
+    const src = getIconSource.getSource();
 
     return (
       <div>
