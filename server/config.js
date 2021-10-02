314c70e51ce8868058e638a4cd07816ae43c47af
alatruwe <49768573+alatruwe@users.noreply.github.com>
2021-10-02 15:31:24 -0700

    add axios and dotenv packages + server config

---
+++ server/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  API_KEY: process.env.API_KEY,
+  API_URL: "http://api.openweathermap.org/data/2.5/",
+  API_FORECAST_ENDPOINT: "forecast?",
+  API_ONECALL_ENDPOINT: "onecall?",
+};
